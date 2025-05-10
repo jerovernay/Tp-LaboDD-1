a711adb1e9f8e40e3d96c2ffab511f42aaa0aa6d
Jero <jeronimo.vernay@gmail.com>
2025-05-10 20:13:20 -0300

    cree el padron con el rango etario

---
+++ Descargas/otros.py
@@ -0,0 +1,76 @@
+import duckdb
+import pandas as pd
+import os
+
+print("Current working directory:", os.getcwd(), '\n')
+print("Files in current directory:", os.listdir(), '\n')
+
+
+#Leo el csv de Bibliotecas Populares
+bibliotecas = pd.read_csv("bibliotecas-populares.csv")
+establecimientos_ed = pd.read_csv("2025.04.08_padroin_oficial_establecimientos_educativos_die.csv", sep=';')
+padron = pd.read_csv("padron_poblacion.csv", dtype={'Area': str})
+
+
+# Tenemos que clasificar el padron en grupos etarios para asi despues hacer las consultas que nos piden respecto a esto
+
+
+def clasificacion_por_grupo_etario_padron(edad):
+    
+    if 0 <= edad <= 2:
+        return "0 a 2"
+    elif 3 <= edad <= 5:
+        return '3 a 5'
+    elif 6 <= edad <= 11:
+        return "6 a 11"
+    elif 12 <= edad <= 18:
+        return '12 a 18'
+    elif 19 <= edad <= 29:
+        return "19 a 29"
+    elif 30 <= edad <= 45:
+        return '30 a 45'
+    elif 46 <= edad <= 60:
+        return '46 a 60'
+    else:
+        return "< 60"
+    
+# Quito la comuna porque no suma en nada y perdemos datos a la hora de agruparla asi.
+# Todos los departamentos que tengan un numero como nombre son leidos como comunas, mientras que el resto ni los lee
+# Yo creo que va a convenir dejar los nombres, pero aca ya tenemos un approach
+
+padron01 = padron.copy()
+
+padron01['Rango etario'] = padron01['Edad'].apply(clasificacion_por_grupo_etario_padron)
+
+padron02 = padron01[padron01['Rango etario'].notna()]
+
+padron03 = padron02.groupby(['Area','Rango etario']).agg({
+    'Casos' : 'sum',
+    })
+
+# Calculamos el total de casos por Area para recalcular los porcentajes
+
+
+padron03['%'] = padron03.groupby(['Area'])['Casos'].transform(lambda x: (x / x.sum()) * 100)
+
+padron03['Acumulado %'] = padron03.groupby(['Area'])['%'].cumsum()
+
+padron03 = padron03.reset_index()[['Rango etario', 'Casos', '%', 'Acumulado %', 'Area']]
+
+# Testeo el nuevo padron
+
+espacios = []
+
+# Pongo espacios entre las columnas, asi no queda todo junto
+
+for comuna, grupo in padron03.groupby('Area'):
+    espacios.append(grupo)  # agregamos el bloque original
+    espacios.append(pd.DataFrame([[''] * len(grupo.columns)] * 2, columns=grupo.columns))  # agregamos 2 filas vacías
+
+padron_final = pd.concat(espacios, ignore_index= True)
+
+# Genera el padron final, lo comento asi no lo creamos devuelta, xlas.
+
+# padron_final.to_csv("padron_prueba02.csv", index=False)
+
+
--- Descargas/test.py
+++ Descargas/test.py
@@ -2,37 +2,27 @@ import duckdb
 import pandas as pd
 import os
 
-os.chdir(r"C:\Users\Dell\Escritorio\Tp-LaboDD-1\Descargas") # Aca pongan el directorio que esten usando, asi con cambiar esto solo ya nos core en cualquier compu 
-print("Current working directory:", os.getcwd(), '\n')
-print("Files in current directory:", os.listdir(), '\n')
+os.chdir(r"C:\Users\jeron\Mis-cosas\facultad descargas\labo de datos\tp-labodd-1\Descargas") # Aca pongan el directorio que esten usando, asi con cambiar esto solo ya nos core en cualquier compu 
+# print("Current working directory:", os.getcwd(), '\n')
+# print("Files in current directory:", os.listdir(), '\n')
 
 
 #Leo el csv de Bibliotecas Populares
 bibliotecas = pd.read_csv("bibliotecas-populares.csv")
-
-con = duckdb.connect()
-con.register("Bibliotecas", bibliotecas)
-
 establecimientos_ed = pd.read_csv("2025.04.08_padroin_oficial_establecimientos_educativos_die.csv", sep=';')
 padron = pd.read_csv("padron_poblacion.csv", dtype={'Area': str})
 
 
-
-# Estoy mirando por arriba los datos
-
-# Veo que las columnas todas tengan el mismo dato
-
-res = con.execute("""
-SELECT DISTINCT fuente, COUNT(*) as cantidad 
-FROM Bibliotecas
-GROUP by fuente
-""").fetchdf()
+con = duckdb.connect()
+con.register("Bibliotecas", bibliotecas)
 
 
-print(res) #Son 1902 datos, si testeamos en los otros es igual
+
 
 # Aca les dejo si quieren ver mas a fondo las columnas de los csv con los tipos de datos, nulls y demas 
 # Hay una banda de datos al pedo
+
+"""
 print("Info sobre Bp:")
 print(bibliotecas.info())  
 
@@ -41,6 +31,8 @@ print(establecimientos_ed.info())
 
 print("Info sobre padron:")
 print(padron.info())
+
+"""
 
 consultaSQL = """
                SELECT Comuna, Area, SUM(CAST(Casos AS DECIMAL)) AS Total_Casos
@@ -52,7 +44,28 @@ consultaSQL = """
 
 dataframeResultado = duckdb.query(consultaSQL).df()
 
-print(dataframeResultado)
+#print(dataframeResultado)
+
+
+
+# Testing sobre lo que espero sea el padron final.
+
+df_padron_prueba = pd.read_csv("padron_prueba02.csv")
+con.register("prueba", df_padron_prueba)
+
+
+
+consulta00 = con.execute(
+"""
+SELECT COUNT(DISTINCT Area) AS cantidad_areas 
+FROM prueba;
+""").fetchdf()
+
+print(consulta00) # 527 como necesitabamos, no se rompe nada
+
+
+
+
 
 ''' Dejo comentado esta parte que estaba en la ultima version del archivo porque es posible que los nombres no se correspondan con la nueva version
 
