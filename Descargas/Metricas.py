86da584d9a3bf56ccfa2199d80951b216979769c
Jero <jeronimo.vernay@gmail.com>
2025-05-21 17:01:32 -0300

    subo las metricas, comentadas y borro la carpeta GQM

--- Descargas/Metricas.py
+++ Descargas/Metricas.py
@@ -1,12 +1,103 @@
+""" Metricas del Analisis de Calidad de Datos para el GQM """
+
+
 import duckdb
 import pandas as pd
 
 
-bibliotecas = pd.read_csv("bibliotecas_populares.csv")
+#Leemos los Csv's originales
+bibliotecas = pd.read_csv("aca ponemos el oficial que usemos para BP")
+establecimientos_ed = pd.read_csv("aca ponemos el oficial que usemos para EE", sep=';')
 
+# Conectamos a Duckdb y registramos 
 con = duckdb.connect()
-
+con.register("EE", establecimientos_ed)
 con.register("BP", bibliotecas)
+
+
+
+"""===== Metricas para el Analisis de Calidad de Datos de Establecimientos Educativos ====="""
+
+print("Veamos los datos en crudo: ")
+establecimientos_ed.info()
+print('\n Nulls en columnas: ')
+print(establecimientos_ed.isnull().sum(), '\n')
+
+
+
+""" Problemas asociado con los Correos """
+
+Registros_totales = len(establecimientos_ed)
+mails_nulls = establecimientos_ed["Mail"].isnull().sum()
+completitud_mails = mails_nulls / Registros_totales * 100
+
+print("\nPorcentaje de establecimientos sin correo electronico: ")
+print(round(completitud_mails,2), '\n')
+
+
+mails_mal_formato_total = """
+/*Cuento los correos validos, los multiplico para convertirlo en porcentaje y 
+ divido sobre el total de mails no nulos*/
+SELECT 
+    COUNT(*) * 100.0 / (
+    SELECT COUNT(*) FROM EE WHERE "Mail" IS NOT NULL
+    ) AS porcentaje_mail_totalmente_invalidos
+FROM EE
+/*Aca debajo, cuento los mails validos*/
+WHERE "Mail" IS NOT NULL
+  AND INSTR("Mail", '@') = 0;
+"""
+
+
+cant_mails_mal_totalmente = con.query(mails_mal_formato_total).to_df()
+print(cant_mails_mal_totalmente, '\n') 
+
+""" Calculamos el porcentaje de mails validos """
+
+# Teniendo en cuenta que los mails son 81.71 validos, 
+# le multiplicamos el porcentaje que tiene al menos un arroba 
+
+porcentaje_EE_mail_valido = (99.65 * 81.71 / 100)
+print(f"Porcentaje de mail validos: {porcentaje_EE_mail_valido: .2f}%", '\n') 
+
+
+
+
+""" Problemas asociado a el domicilio """
+
+# Porcentaje de domicilios sin numero 
+
+sin_numero = establecimientos_ed["Domicilio"].apply(lambda x: not any(char.isdigit() for char in str(x)))
+porcentaje_sin_numero = sin_numero.mean() * 100
+
+print(f"\nPorcentaje de Domicilios sin Numero: {porcentaje_sin_numero: .2f}%", '\n') 
+
+
+# Porcentaje de domicilios con sintexis invalida("falsos")
+
+chars_invalidos = {'?', '%', '$', '#', '@', '*', ';','^', '!', '<', '>'}
+patron_invalido = {'', ' '}
+
+def es_invalido(x):
+    
+    x_str = str(x).strip().lower()      #Normalizacion del String
+    
+    return(x_str in patron_invalido or  any(y in str(x) for y in chars_invalidos)) # Vemos si esta vacio o tiene cosas que no deben estar
+
+
+invalidos = establecimientos_ed["Domicilio"].apply(es_invalido)
+porcentaje_invalido = invalidos.mean() * 100
+
+print(f"\nPorcentaje de Domicilios Invalidos: {porcentaje_invalido: .2f}%", '\n')
+
+
+
+
+
+
+"""===== Metricas para el Analisis de Calidad de Datos de Bibliotecas Populares ====="""
+
+
 
 bibliotecas.info()
 print('\n Nulls en columnas: ')
@@ -115,18 +206,3 @@ casos_problematicos = combinaciones_prov_loc[combinaciones_prov_loc > 1]
 print(f"Combinaciones 'provincia' + 'localidad' con múltiples departamentos: {len(casos_problematicos)}")
 print("\nEjemplos de casos problematicos: ")
 print(casos_problematicos.head())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
