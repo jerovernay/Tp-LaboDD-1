4b6d7ef5a9b19c647914348ce049f1b8ee16a599
gastonarida <105611384+gastonarida@users.noreply.github.com>
2025-05-20 21:08:30 -0300

    Update primer_grafico.py

--- Descargas/Graficos/primer_grafico.py
+++ Descargas/Graficos/primer_grafico.py
@@ -2,9 +2,9 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-BP = pd.read_csv(r"C:\Users\gasto\Downloads\BP_limpio.csv")
-EE = pd.read_csv(r"C:\Users\gasto\Downloads\EE_limpio.csv")
-Padron = pd.read_csv(r"C:\Users\gasto\Downloads\labo\padron_limpio.csv")
+BP = pd.read_csv(r"C:\Users\gasto\OneDrive\Documents\tp_labo\BP_limpio.csv")
+EE = pd.read_csv(r"C:\Users\gasto\OneDrive\Documents\tp_labo\EE_limpio_final_usando2daopcion.csv")
+Padron = pd.read_csv(r"C:\Users\gasto\OneDrive\Documents\tp_labo\Padron_limpio_final.csv")
 
 provincias = BP['provincia'].unique()
 
@@ -30,9 +30,12 @@ sns.set(
 
 ax = sns.barplot(data=df, x='Provincia', y='Cantidad', palette='plasma') #grafico
 
+plt.ylabel("Bibliotecas populares", fontsize=14)
+plt.xlabel("Provincia", fontsize=14)
+
 plt.xticks(rotation=45, ha="right")  # Rotación y alineación del texto para que se puedan ver el nombre de cada una de las provincias
 
-plt.title("Bibliotecas por provincia") # Titulo del grafico
+plt.title("Bibliotecas populares por provincia", fontsize=18) # Titulo del grafico
 
 for i, valor in enumerate(df["Cantidad"]):
     ax.text(i, valor + 0.5, str(valor), ha="center", fontsize=12)  # Usamos esto para poner el numero arriba de cada barra, asi es mas claro
