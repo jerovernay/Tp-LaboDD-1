50ad66189dff093190e39b33cdf8c8089d66b7e7
gastonarida <105611384+gastonarida@users.noreply.github.com>
2025-05-21 15:20:32 -0300

    codigo de limpieza para Padron

--- Descargas/Desarrollo/codigo/Padron_cambios_juntos.py
+++ Descargas/Desarrollo/codigo/Padron_cambios_juntos.py
@@ -1,6 +1,9 @@
+# codigos de limpieza para padron, usando el archivo corregido para la columna 15
 import pandas as pd
 
-Padron = pd.read_csv(r"C:\Users\gasto\OneDrive\Documents\tp_labo\Padron2.csv")
+Padron = pd.read_csv(r'C:\Users\gasto\Downloads\padron_poblacion.csv')
+
+Padron.loc[Padron['departamento'] == 'COMUNA 15', 'id_departamento'] = Padron.loc[Padron['departamento'] == 'COMUNA 15', 'id_departamento'].replace(2105, 2115)
 
 def clasificacion_por_grupo_etario_padron(edad):
     
@@ -37,48 +40,41 @@ Padron = Padron.groupby(['id_departamento','Rango etario']).agg({
 
 # Calculamos el total de casos por Area para recalcular los porcentajes
 
-
-Padron['%'] = Padron.groupby(['id_departamento'])['casos'].transform(lambda x: (x / x.sum()) * 100)
-
-Padron['Acumulado %'] = Padron.groupby(['id_departamento'])['%'].cumsum()
-
-Padron = Padron.reset_index()[['id_departamento', 'Rango etario', 'casos', '%', 'Acumulado %']]
+Padron = Padron.reset_index()[['id_departamento', 'Rango etario', 'casos']]
 
 # Testeo el nuevo padron
 
 espacios = []
 
-# Pongo espacios entre las columnas, asi no queda todo junto
-
 for comuna, grupo in Padron.groupby('id_departamento'):
     espacios.append(grupo)  # agregamos el bloque original
-    # espacios.append(pd.DataFrame([[''] * len(grupo.columns)] * 2, columns=grupo.columns))  # agregamos 2 filas vacías
+
 
 Padron = pd.concat(espacios, ignore_index= True)
 
-# Padron.rename(columns={'Area': 'id_departamento'}, inplace=True)
 
 # Reemplazamos los id_departamento de comunas y de Ushuaia
 
 print(Padron['id_departamento'].dtype)
 
 Padron['id_departamento'] = Padron['id_departamento'].replace({
-    '94015': '94014',
-    '94008': '94007',
-    '02007': '02101',
-    '02014': '02102',
-    '02021': '02103',
-    '02028': '02104',
-    '02035': '02105',
-    '02042': '02106',
-    '02049': '02107',
-    '02056': '02108',
-    '02063': '02109',
-    '02070': '02110',
-    '02077': '02111',
-    '02084': '02112',
-    '02091': '02113',
-    '02098': '02114'
+    94015: 94014,
+    94008: 94007,
+    2007: 2101,
+    2014: 2102,
+    2021: 2103,
+    2028: 2104,
+    2035: 2105,
+    2042: 2106,
+    2049: 2107,
+    2056: 2108,
+    2063: 2109,
+    2070: 2110,
+    2077: 2111,
+    2084: 2112,
+    2091: 2113,
+    2098: 2114,
+    2105: 2115
 })
 
 
@@ -108,25 +104,24 @@ Padron['Rango etario'] = pd.Categorical(
 dfs_ordenados = []
 
 for depto in deptos:
-    # Filtrar datos del departamento actual
+    # Filtra datos del departamento actual
     df_depto = Padron[Padron['id_departamento'] == depto].copy()
     
-    # Ordenar por rango etario (usando el orden categórico)
+    # Ordena por rango etario (usando el orden categórico)
     df_depto = df_depto.sort_values('Rango etario')
     
-    df_depto['Acumulado %'] = df_depto['%'].cumsum()
     
-    # Agregar al listado
+    # Agrega al listado
     dfs_ordenados.append(df_depto)
     
-    # Agregar dos filas vacías (excepto después del último departamento)
+    # Agrega dos filas vacías (excepto después del último departamento)
     if depto != deptos[-1]:
         dfs_ordenados.append(pd.DataFrame([{}] * 2))  # Filas vacías
 
-# Concatenar todos los DataFrames
+# Concatena todos los DataFrames
 df_final = pd.concat(dfs_ordenados, ignore_index=True)
 
-# Resetear índice
+# Resetea índice
 df_final.reset_index(drop=True, inplace=True)
 
-df_final.to_csv('Padron_limpio.csv', index=False) 
+df_final.to_csv('Padron_limpio_final.csv', index=False) 
